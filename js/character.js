36fd70dfd1bd2ab07828d3971be78aaca9aebd89
Noureldin <noureldinyosri@gmail.com>
2016-12-19 09:41:05 +0200

    finished character texture .. motion remains

--- js/character.js
+++ js/character.js
@@ -3,8 +3,10 @@ function character() {
 	var points = [],vTexCoord = [],colors = [];
 	var points_buffer,texture,TexCoord_buffer,colors_buffer;
 	var image;
-	var theta = vec3(10,90,-30),thetaLoc;
+	var theta = vec3(0,90,0),thetaLoc;
 
+	var blue = [vec2(447,70),vec2(447,117),vec2(480,117),vec2(480,70)];
+	var red = [vec2(580,255),vec2(580,290),vec2(540,290),vec2(580,255)];
 	var TexCoord = [
 		[[vec2(1400,1030),vec2(1255,1170),vec2(1032,950),vec2(1250,740)]
 		,[vec2(1040,525),vec2(850,735),vec2(1032,950),vec2(1250,740)]
@@ -13,11 +15,17 @@ function character() {
 		,[vec2(1255,1170),vec2(1400,1020),vec2(1250,740),vec2(1032,950)]
 		,[vec2(540,162),vec2(393,309),vec2(607,524),vec2(756,375)]
 		] // head
-		,[[]] // torso
-		,[] // lef hand
-		,[] // right hand
-		,[] // left leg
-		,[] // right leg
+		,[[vec2(5,535),vec2(240,535),vec2(240,347),vec2(5,347)]
+		,[vec2(55,730),vec2(250,730),vec2(250,530),vec2(55,530)]
+		,blue
+		,[vec2(820,740),vec2(609,950),vec2(820,1170),vec2(1040,950)]
+		,[vec2(240,347),vec2(5,347),vec2(5,535),vec2(240,535)]
+		,[vec2(55,1120),vec2(245,1120),vec2(245,925),vec2(60,925)]
+		] // torso
+		,[red,red,red,red,red,red] // lef hand
+		,[red,red,red,red,red,red] // right hand
+		,[blue,blue,blue,blue,blue,blue] // left leg
+		,[blue,blue,blue,blue,blue,blue] // right leg
 	];
 
 	function Cube(idx){
@@ -35,14 +43,10 @@ function character() {
 		for ( var i = 0; i < corners.length; ++i ) {
 			var r = vertices[corners[i]];
 			points.push(r);
-		//	colors.push(color);
-			console.log(idx + " " + idx2 + " " + TexCoor[i])
 			console.log(TexCoord[idx])
 			var v = scale(1,TexCoord[idx][idx2][TexCoor[i]]);
 			v[0] /= 1482; v[1] /= 1173;
-			//v = vec2(v[1],v[0]);
 			vTexCoord.push(v);
-//			vTexCoord.push(vec2(0.5 - corners[i][0],0.5 - corners[i][1] ));
 		}
 	}
 
@@ -54,22 +58,17 @@ function character() {
 	}
 
 	function create_scene() {
-		var head = node(0,"head",[0.4,0.03,0,0],0.5,0.5,0.5); //0.1,0.1,0.1
-		var torso = node(1,"torso",[0.4,-0.2,0,0],0.1,0.3,0.1);
+		var head = node(0,"head",[0.4,0.0,0,0],0.2,0.1,0.1);
+		var torso = node(1,"torso",[0.4,-0.2,0,0], 0.3,0.3,0.1); // 
 		var left_hand = node(2,"left hand",[0.4,-0.1,-0.1,0],0.07,0.1,0.07);
 		var right_hand = node(3,"right hand",[0.4,-0.1,0.1,0],0.07,0.1,0.07);
 		var left_leg = node(4,"left leg",[0.4,-0.35,-0.1,0],0.07,0.1,0.07);
 		var right_leg = node(5,"right leg",[0.4,-0.35,0.1,0],0.07,0.1,0.07);
-		/*
 		torso.add_child(head);
 		torso.add_child(left_hand);
 		torso.add_child(right_hand);
 		torso.add_child(left_leg);
 		torso.add_child(right_leg);
-		torso.traverse();
-		console.log(points)
-		*/
-		//head.traverse();
 		torso.traverse();
 	}
 
