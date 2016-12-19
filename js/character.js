027a48d6303aec98d5ef0f5215975d3656735158
Noureldin <noureldinyosri@gmail.com>
2016-12-19 09:15:29 +0200

    finished head

--- js/character.js
+++ js/character.js
@@ -3,24 +3,45 @@ function character() {
 	var points = [],vTexCoord = [],colors = [];
 	var points_buffer,texture,TexCoord_buffer,colors_buffer;
 	var image;
-	var theta = vec3(0,0,0),thetaLoc;
-	var red = vec4(1,0,0,1),green = vec4(0,1,0,1),blue = vec4(0,0,1,1);
-	function Cube(){
-		quad( 1, 0, 3, 2,red); // left face
-		quad( 2, 3, 7, 6,green); // front face
-		quad( 3, 0, 4, 7,blue); // bottom face
-		quad( 6, 5, 1, 2,add(red,blue));  // up face
-		quad( 4, 5, 6, 7,add(red,green)); // right face
-		quad( 5, 4, 0, 1,add(blue,green)); // back face
+	var theta = vec3(10,90,-30),thetaLoc;
+
+	var TexCoord = [
+		[[vec2(1400,1030),vec2(1255,1170),vec2(1032,950),vec2(1250,740)]
+		,[vec2(1040,525),vec2(850,735),vec2(1032,950),vec2(1250,740)]
+		,[vec2(820,740),vec2(609,950),vec2(820,1170),vec2(1040,950)]
+		,[vec2(1260,300),vec2(1038,520),vec2(1250,735),vec2(1472,513)]
+		,[vec2(1255,1170),vec2(1400,1020),vec2(1250,740),vec2(1032,950)]
+		,[vec2(540,162),vec2(393,309),vec2(607,524),vec2(756,375)]
+		] // head
+		,[[]] // torso
+		,[] // lef hand
+		,[] // right hand
+		,[] // left leg
+		,[] // right leg
+	];
+
+	function Cube(idx){
+		quad( 1, 0, 3, 2,idx,0); // left face
+		quad( 2, 3, 7, 6,idx,1); // front face
+		quad( 3, 0, 4, 7,idx,2); // bottom face
+		quad( 6, 5, 1, 2,idx,3);  // up face
+		quad( 4, 5, 6, 7,idx,4); // right face
+		quad( 5, 4, 0, 1,idx,5); // back face
 	}
 
-	function quad(a,b,c,d,color) {
+	function quad(a,b,c,d,idx,idx2) {
 		var corners = [ a, b, c, a, c, d ];
+		var TexCoor = [0 , 1, 2 ,0 , 2 ,3];
 		for ( var i = 0; i < corners.length; ++i ) {
 			var r = vertices[corners[i]];
 			points.push(r);
-			colors.push(color);
-
+		//	colors.push(color);
+			console.log(idx + " " + idx2 + " " + TexCoor[i])
+			console.log(TexCoord[idx])
+			var v = scale(1,TexCoord[idx][idx2][TexCoor[i]]);
+			v[0] /= 1482; v[1] /= 1173;
+			//v = vec2(v[1],v[0]);
+			vTexCoord.push(v);
 //			vTexCoord.push(vec2(0.5 - corners[i][0],0.5 - corners[i][1] ));
 		}
 	}
@@ -33,13 +54,13 @@ function character() {
 	}
 
 	function create_scene() {
-		var head = node("head",[0.4,-0.15,0,0],0.1,0.1,0.1);
-		var torso = node("torso",[0.4,-0.25,0,0],0.1,0.1,0.1);
-		var left_hand = node("left hand",[0.4,-0.25,-0.01,0],0.1,0.1,0.1);
-		var right_hand = node("right hand",[0.4,-0.25,0.01,0],0.1,0.1,0.1);
-		var left_leg = node("left leg",[0.4,-0.35,-0.1,0],0.1,0.1,0.1);
-		var right_leg = node("right leg",[0.4,-0.35,0.1,0],0.1,0.1,0.1);
-
+		var head = node(0,"head",[0.4,0.03,0,0],0.5,0.5,0.5); //0.1,0.1,0.1
+		var torso = node(1,"torso",[0.4,-0.2,0,0],0.1,0.3,0.1);
+		var left_hand = node(2,"left hand",[0.4,-0.1,-0.1,0],0.07,0.1,0.07);
+		var right_hand = node(3,"right hand",[0.4,-0.1,0.1,0],0.07,0.1,0.07);
+		var left_leg = node(4,"left leg",[0.4,-0.35,-0.1,0],0.07,0.1,0.07);
+		var right_leg = node(5,"right leg",[0.4,-0.35,0.1,0],0.07,0.1,0.07);
+		/*
 		torso.add_child(head);
 		torso.add_child(left_hand);
 		torso.add_child(right_hand);
@@ -47,27 +68,28 @@ function character() {
 		torso.add_child(right_leg);
 		torso.traverse();
 		console.log(points)
-
+		*/
+		//head.traverse();
+		torso.traverse();
 	}
 
 	function bind_data(send_data) {
 		points_buffer = bind(points,points_buffer,"vPosition",4,character_program,send_data);
-		colors_buffer = bind(colors,colors_buffer,"vColor",4,character_program,send_data);
 
-//		TexCoord_buffer = bind(vTexCoord,TexCoord_buffer,"vTexCoord",2,character_program ,send_data);
-/*
+		TexCoord_buffer = bind(vTexCoord,TexCoord_buffer,"vTexCoord",2,character_program ,send_data);
+
 		if(image == undefined){
 			image = new Image();
-			url = "brick_texture3417.jpg";
+			url = "character018-1.jpg";
 			image.crossOrigin = "";
 			image.src = url;
 			image.onload = function() {
-				texture = configureTexture(image, texture, "texture", gl.TEXTURE3, 3 ,character_program ,send_data)
+				texture = configureTexture(image, texture, "texture", gl.TEXTURE2, 2 ,character_program ,send_data)
 				render();
 			};
 		}
-		else texture = configureTexture(image, texture,"texture", gl.TEXTURE3, 3 ,character_program ,send_data);
-*/
+		else texture = configureTexture(image, texture,"texture", gl.TEXTURE2, 2 ,character_program ,send_data);
+
 		thetaLoc = gl.getUniformLocation( character_program, "theta" );
 	}
 		function render()
@@ -88,8 +110,8 @@ function character() {
 	create_character();
 
 
-	function node(s,dis,w,h,l) {
-		var name = s;
+	function node(idx,s,dis,w,h,l) {
+		var name = s,index = idx;
 		var trans_matrix = mat4(1),pos = dis,width = w,height = h,length = l;
 		var children = [];
 		function traverse(M) {
@@ -102,7 +124,7 @@ function character() {
 			}
 		}
 		function print_node() {
-			Cube();
+			Cube(index);
 			for(var i = points.length - 36;i < points.length;i++){
 				var r = points[i];
 				r = scale(1,r);
